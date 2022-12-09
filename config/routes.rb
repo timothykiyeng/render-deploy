3e42bf1dffd647380e499ecaa0e61d7ce63f5519
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-09 14:20:42 +0300

    ft: routes - shows routes to each resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :skill_sets
   resources :comments
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :skill_sets, only: [:index, :show, :update]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
 end
