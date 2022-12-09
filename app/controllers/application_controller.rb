4577667efff8c801a947bc242dee69f783be6900
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-09 15:25:36 +0300

    ft: authorize - authorize a user if in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   include ActionController::Cookies
 
-  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
 
+  private
 
-  def render_unprocessable_entity(invalid)
-    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
   end
+
+  def render_unprocessable_entity_response(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,27 +2,20 @@ class UsersController < ApplicationController
   skip_before_action :authorize, only: :create
 
   def create
-    user = User.create(user_params)
-    if user.valid?
+      user = User.create!(user_params)
       session[:user_id] = user.id
       render json: user, status: :created
-    else
-      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-    end
-   end
+  end
+
+  def show
+      render json: @current_user
+  end
 
 
-def show
-	user = User.find(session[:user_id])
-	render json: user
-end
   private
 
   def user_params
-    params.permit(:username, :password, :password_confirmation)
+      params.permit(:username, :password, :password_confirmation)
   end
-  
-  def authorize
-		return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
-	end
+
 end
