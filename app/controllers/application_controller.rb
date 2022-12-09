51bcd083540e2010947f256ff64b33c7db585b75
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-09 14:32:51 +0300

    ft: authorize - authorize a user if in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,25 @@ class ApplicationController < ActionController::Base
   include ActionController::Cookies
 
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
-  before_action :authorize
+  include ActionController::Helpers
+    include Response
+    helper_method :current_user
+
+    def current_user #We use this action to monitor who is logged in based on the session
+      if session[:author_id]
+          @current_user = User.find(session[:user_id])
+      end
+  end
 
 
   private
 
   def authorize
-    @current_user = User.find_by(id: session[:user_id])
+    if !current_user
+      json_response({message:'You are not authorized to perform this action. You have to log in'}, status = 401)
+    end
+end
 
-    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
-  end
 
   def render_unprocessable_entity(invalid)
     render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
