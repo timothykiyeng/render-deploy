c3088237a334153dd490a47484a7c095af70d8c4
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-09 16:47:42 +0300

    ft: authorize - authorize a user if in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,15 @@ class ApplicationController < ActionController::API
 
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
-  before_action :authorize
+  # before_action :authorize
 
-  private
+  # private
 
-  def authorize
-    @current_user = User.find_by(id: session[:user_id])
+  # def authorize
+  #   @current_user = User.find_by(id: session[:user_id])
 
-    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
-  end
+  #   render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+  # end
 
   def render_unprocessable_entity_response(invalid)
       render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
