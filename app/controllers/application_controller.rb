17c22e8095960086e27aecabc093630c513c4dc8
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-09 14:59:28 +0300

    ft: authorize - authorize a user if in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,7 @@ class ApplicationController < ActionController::Base
   include ActionController::Cookies
 
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
-  include ActionController::Helpers
-    include Response
-    helper_method :current_user
 
-    def current_user #We use this action to monitor who is logged in based on the session
-      if session[:author_id]
-          @current_user = User.find(session[:user_id])
-      end
-  end
-
-
-  private
-
-  def authorize
-    if !current_user
-      json_response({message:'You are not authorized to perform this action. You have to log in'}, status = 401)
-    end
-end
 
 
   def render_unprocessable_entity(invalid)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,27 @@ class UsersController < ApplicationController
   skip_before_action :authorize, only: :create
 
   def create
-    user = User.create!(user_params)
-    session[:user_id] = user.id
-    render json: user, status: :created
-  end
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+   end
 
-  def show
-    render json: @current_user
-  end
 
+def show
+	user = User.find(session[:user_id])
+	render json: user
+end
   private
 
   def user_params
     params.permit(:username, :password, :password_confirmation)
   end
+  
+  def authorize
+		return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+	end
 end
